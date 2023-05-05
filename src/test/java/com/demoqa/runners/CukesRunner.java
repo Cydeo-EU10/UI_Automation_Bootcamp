048f2038f7a86df3e213df1efc640122e94a4814
OscarSah <oscar@cybertekschool.com>
2023-05-05 22:37:07 +0300

    add: slider  bar verification

--- src/test/java/com/demoqa/runners/CukesRunner.java
+++ src/test/java/com/demoqa/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/demoqa/step_definitions",
         dryRun = false,
-        tags = "@progressBar",
+        tags = "@slider",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
--- src/test/java/com/demoqa/step_definitions/SliderSteps.java
+++ src/test/java/com/demoqa/step_definitions/SliderSteps.java
@@ -9,17 +9,19 @@ import org.openqa.selenium.interactions.Actions;
 
 public class SliderSteps {
 
+    SliderPage sliderPage = new SliderPage();
+    Actions actions = new Actions(Driver.getDriver());
 
-
+    int sliderBarAfterValue;
     @When("make some slider actions to {string}")
     public void makeSomeSliderActionsTo(String input) {
-
+        actions.dragAndDropBy(sliderPage.sliderInput, 20,0).perform();
     }
 
     @Then("verify slider value {string}")
     public void verifySliderValue(String outcome) {
 
-
-
+        sliderBarAfterValue = Integer.parseInt(sliderPage.inputValue.getAttribute("value"));
+        System.out.println("from the slider bar itself : " + sliderPage.sliderInput.getAttribute("value") );
     }
 }
