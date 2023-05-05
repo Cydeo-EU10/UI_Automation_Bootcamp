7068af0c96e983c76bf3f06a5f7141a008e0c557
OscarSah <oscar@cybertekschool.com>
2023-05-05 21:49:05 +0300

    add: progress bar steps

--- src/test/java/com/demoqa/step_definitions/ProgressBarSteps.java
+++ src/test/java/com/demoqa/step_definitions/ProgressBarSteps.java
@@ -12,16 +12,19 @@ import java.time.Duration;
 
 public class ProgressBarSteps {
 
-
+    ProgressBarPage progressBarPage = new ProgressBarPage();
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(),Duration.ofSeconds(10));
 
     @When("click startStopButton")
     public void click_start_stop_button() {
-
+        progressBarPage.startStopButton.click();
     }
 
     @When("progress bar continue until value is {string}")
     public void progress_bar_continue_until_value_is(String value) {
 
+        wait.pollingEvery(Duration.ofMillis(10));
+        wait.until(ExpectedConditions.attributeToBe(progressBarPage.progressBar,"aria-valuenow",value));
 
     }
 
